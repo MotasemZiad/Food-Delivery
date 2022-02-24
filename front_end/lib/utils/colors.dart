9bf95d22cedfc87df23205ec69bff1e20d396d1d
Motasem Z. AbuNema <52855540+MotasemZiad@users.noreply.github.com>
2022-02-24 17:34:07 +0200

    intial commit

---
+++ front_end/lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+const Color textColor = Color(0xFFccc7c5);
+const Color mainColor = Color(0xFF89dad0);
+// final Color mainColor = const Color(0xFFfa7552);
+const Color iconColor1 = Color(0xFFffd28d);
+const Color iconColor2 = Color(0xFFfcab88);
+const Color paraColor = Color(0xFF8f837f);
+const Color buttonBackgroundColor = Color(0xFFf7f6f4);
+const Color signColor = Color(0xFFa9a29f);
+const Color titleColor = Color(0xFF5c524f);
+const Color mainBlackColor = Color(0xFF332d2b);
+// final Color yellowColor = const Color(0xFFfa7552);
+const Color yellowColor = Color(0xFFffd379);
---
+++ front_end/lib/utils/constants.dart
@@ -0,0 +1,4 @@
+const laravelVersion = '8.61.0';
+const phpVersion = '7.3.2';
+const dartVersion = '2.16.1';
+const flutterVersion = '2.10.2';
